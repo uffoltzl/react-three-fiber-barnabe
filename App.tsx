e700501af49db62af10745839e13cb0f59b438ce
uffoltzl <l.ufz@hotmail.fr>
2023-03-14 23:15:18 +0100

    background with shaders

--- App.tsx
+++ App.tsx
@@ -1,16 +1,57 @@
 import { Canvas, useFrame, useLoader } from "@react-three/fiber/native";
-import { MutableRefObject, Suspense, useLayoutEffect, useRef } from "react";
+import {
+  MutableRefObject,
+  Suspense,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+} from "react";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { TextureLoader } from "expo-three";
 import * as THREE from "three";
 import { BufferGeometry, Material, Mesh } from "three";
 
+import backgroundVertexShader from "./shaders/backgroundVertexShader";
+import backgroundFragmentShader from "./shaders/backgroundFragmentShader";
+
 const Background = () => {
+  const mesh: MutableRefObject<Mesh<
+    BufferGeometry,
+    Material | Material[]
+  > | null> = useRef(null);
+
+  const uniforms = useMemo(
+    () => ({
+      u_intensity: {
+        value: 0.3,
+      },
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    []
+  );
+
+  useFrame((state) => {
+    const { clock } = state;
+    if (mesh && mesh.current && mesh.current.material) {
+      // @ts-ignore
+      mesh.current.material.uniforms.u_time.value =
+        0.4 * clock.getElapsedTime();
+    }
+  });
+
   return (
-    <mesh scale={100}>
-      <sphereGeometry />
-      <meshBasicMaterial color="#2645e0" side={THREE.BackSide} />
+    <mesh ref={mesh} scale={50}>
+      <icosahedronGeometry args={[2, 20]} />
+      <shaderMaterial
+        fragmentShader={backgroundFragmentShader}
+        vertexShader={backgroundVertexShader}
+        uniforms={uniforms}
+        wireframe={false}
+        side={THREE.BackSide}
+      />
     </mesh>
   );
 };
@@ -66,7 +107,6 @@ const Barnabe = () => {
 const App = () => {
   return (
     <Canvas shadows>
-      <fog attach="fog" args={["#4479e1", 0, 200]} />
       <ambientLight intensity={0.1} />
       <directionalLight position={[0, 1, 0]} intensity={0.7} />
       <Suspense fallback={null}>
