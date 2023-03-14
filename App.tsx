508a8f99a961c00b1891637103a305c3537340ae
uffoltzl <l.ufz@hotmail.fr>
2023-03-14 21:23:21 +0100

    barnabe is there

--- App.tsx
+++ App.tsx
@@ -1,17 +1,53 @@
-import { Canvas } from "@react-three/fiber/native";
-import { Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber/native";
+import { Suspense, useLayoutEffect } from "react";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { TextureLoader } from "expo-three";
+import * as THREE from "three";
+
+const Barnabe = () => {
+  const [base, normal, rough] = useLoader(TextureLoader, [
+    require("./assets/WhaleShark/WhaleShark_Base_Color.png"),
+    require("./assets/WhaleShark/WhaleShark_Normal.png"),
+  ]);
+
+  const material = useLoader(
+    MTLLoader,
+    require("./assets/WhaleShark/WhaleShark.mtl")
+  );
+
+  const obj = useLoader(
+    OBJLoader,
+    require("./assets/WhaleShark/WhaleShark.obj"),
+    (loader) => {
+      material.preload();
+      loader.setMaterials(material);
+    }
+  );
+
+  useLayoutEffect(() => {
+    obj.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material.map = base;
+        child.material.normalMap = normal;
+        child.material.roughnessMap = rough;
+      }
+    });
+  }, [obj]);
+
+  return (
+    <mesh>
+      <primitive object={obj} scale={0.5} />
+    </mesh>
+  );
+};
 
 const App = () => {
   return (
     <Canvas>
-      <ambientLight />
       <pointLight position={[10, 10, 10]} />
-
       <Suspense fallback={null}>
-        <mesh>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-        </mesh>
+        <Barnabe />
       </Suspense>
     </Canvas>
   );
